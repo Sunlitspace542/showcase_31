37854ac4fe9d40d31a4c417319dd727c78e4f162
sunlitspace542 <rabbidsluis123@gmail.com>
2021-07-23 16:15:04 -0400

    Add code

---
+++ README.txt
@@ -0,0 +1,11 @@
+Officially released source code, anybody who says they leaked this code is a liar.
+
+
+Anyways the building process is the same as regular ass decomp lol.
+
+
+
+And yes this project (probably) wont be coming back.
+
+
+-bonmario
---
+++ levels/intro/header.h
@@ -0,0 +1,54 @@
+#ifndef INTRO_HEADER_H
+#define INTRO_HEADER_H
+
+#include "types.h"
+
+// geo
+extern const GeoLayout intro_geo_0002D0[];
+extern const GeoLayout intro_geo_mario_head_regular[];
+extern const GeoLayout intro_geo_mario_head_dizzy[];
+extern const GeoLayout intro_geo_000414[];
+
+// leveldata
+extern Lights1 beytahtitle_Red_f3d_lights;
+extern Lights1 beytahtitle_Blue_f3d_lights;
+extern Lights1 beytahtitle_Yellow_f3d_lights;
+extern Lights1 beytahtitle_White_f3d_lights;
+extern Lights1 beytahtitle_Green_f3d_lights;
+extern Lights1 beytahtitle_Purple_f3d_lights;
+extern Vtx beytahtitle_Logo_mesh_vtx_0[227];
+extern Gfx beytahtitle_Logo_mesh_tri_0[];
+extern Vtx beytahtitle_Logo_mesh_vtx_1[258];
+extern Gfx beytahtitle_Logo_mesh_tri_1[];
+extern Vtx beytahtitle_Logo_mesh_vtx_2[203];
+extern Gfx beytahtitle_Logo_mesh_tri_2[];
+extern Vtx beytahtitle_Logo_mesh_vtx_3[250];
+extern Gfx beytahtitle_Logo_mesh_tri_3[];
+extern Vtx beytahtitle_Logo_mesh_vtx_4[207];
+extern Gfx beytahtitle_Logo_mesh_tri_4[];
+extern Vtx beytahtitle_Logo_mesh_vtx_5[462];
+extern Gfx beytahtitle_Logo_mesh_tri_5[];
+extern Gfx mat_beytahtitle_Red_f3d[];
+extern Gfx mat_beytahtitle_Blue_f3d[];
+extern Gfx mat_beytahtitle_Yellow_f3d[];
+extern Gfx mat_beytahtitle_White_f3d[];
+extern Gfx mat_beytahtitle_Green_f3d[];
+extern Gfx mat_beytahtitle_Purple_f3d[];
+extern Gfx beytahtitle_Logo_mesh[];
+
+extern const Gfx intro_seg7_dl_0700C6A0[];
+extern const f32 intro_seg7_table_0700C790[];
+extern const f32 intro_seg7_table_0700C880[];
+
+// script
+extern const LevelScript level_intro_splash_screen[];
+extern const LevelScript level_intro_mario_head_regular[];
+extern const LevelScript level_intro_mario_head_dizzy[];
+extern const LevelScript level_intro_entry_4[];
+extern const LevelScript script_intro_L1[];
+extern const LevelScript script_intro_L2[];
+extern const LevelScript script_intro_L3[];
+extern const LevelScript script_intro_L4[];
+extern const LevelScript script_intro_L5[];
+
+#endif
